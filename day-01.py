7f81a2546bd9d9ac247b2b3fa0052efbcd582cb0
SRanuluge <sranuluge.dev@gmail.com>
2024-05-14 22:15:10 +1200

    1st day init commit

---
+++ day-01.py
@@ -0,0 +1,32 @@
+# print("hello " + input("What is your name?") + "!")
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+# Write your code below this line 👇
+
+
+# name = input()
+# string_length = len(name)
+# print(string_length)
+
+# Variable naming readable
+
+# final projects
+
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
