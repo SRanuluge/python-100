4f559ab05f4b56125636e161bba5ffea5b86a6f4
SRanuluge <sranuluge.dev@gmail.com>
2024-05-15 10:28:44 +1200

    data types

---
+++ day-02.py
@@ -0,0 +1,14 @@
+# Data types, Numbers, Operations, Type Conventions, f-Strings
+#Primitive data type 
+
+# integer boolean float
+
+# mystery = 734_529.678 => Float
+
+# len is not working with integers
+
+print("Hello"[0])
+
+# type()
+
+#type conversion or casting
