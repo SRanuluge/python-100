bb391bf7a1764f75e30bb9bace9cfa8417dc4309
SRanuluge <sranuluge.dev@gmail.com>
2024-05-15 14:10:13 +1200

    Order of operators

--- day-02.py
+++ day-02.py
@@ -7,8 +7,21 @@
 
 # len is not working with integers
 
-print("Hello"[0])
+# print("Hello"[0])
 
 # type()
 
 #type conversion or casting
+
+# -------------------------------------------------------
+
+# 2**3 => 2*2*2
+
+#Order => PEMDAS => () ** * / + -
+
+# * /  => equaly important
+#+ -  => equaly important
+
+# calculation goes left to right 
+# print(3 * 3 + 3 /3 -3) => 7
+# round()
