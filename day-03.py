94369d0b297cc44aa13f8f223fcbb6dcc5a2f4a3
SRanuluge <sranuluge.dev@gmail.com>
2024-05-16 19:56:20 +1200

    day 3

---
+++ day-03.py
@@ -0,0 +1,22 @@
+# Conditional statements, logical operators, code blocks and scope
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#-----------------------------------------------------------
+
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if (number % 2) == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
